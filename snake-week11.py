b14bdad25772362c8379ff5adcc648e745653365
Bilong Chen <andrew.blchen@gmail.com>
2020-11-07 20:16:45 -0800

    Update snake-week11.py

--- snake-week11.py
+++ snake-week11.py
@@ -1,4 +1,5 @@
 import turtle
+import time
 
 # Step 1: Create a screen, and configure the screen
 screen = turtle.Screen()
@@ -26,37 +27,63 @@ head.direction = "stop"
 def move_up():
     if head.direction != "down":
         head.direction = "up"
-        y = head.ycor()
-        head.sety(y + 20)
         
     print("snake is moving up")
 
 def move_down():
     if head.direction != "up":
         head.direction = "down"
-        y = head.ycor()
-        head.sety(y - 20)
 
     print("snake is moving down")
 
 def move_left():
     if head.direction != "right":
         head.direction = "left"
-        x = head.xcor()
-        head.setx(x - 20)
+
     print("snake is moving left")
 
 def move_right():
     if head.direction != "left":
         head.direction = "right"
+
+    print("snake is moving right")
+
+def move():
+    if head.direction == 'up':
+        y = head.ycor()
+        head.sety(y + 20)
+    if head.direction == 'down':
+        y = head.ycor()
+        head.sety(y - 20)
+    if head.direction == 'left':
+        x = head.xcor()
+        head.setx(x - 20)
+    if head.direction == 'right':
         x = head.xcor()
         head.setx(x + 20)
-    print("snake is moving right")
     
 screen.listen()
 screen.onkeypress(move_up, "Up")
 screen.onkeypress(move_left, "Left")
 screen.onkeypress(move_down, "Down")
 screen.onkeypress(move_right, "Right")
+
+body = []
+delay = 1
+
+while True:
+    screen.update()
+    
+    if head.distance(food) < 20:
+        tail = turtle.Turtle()
+        tail.speed(0)
+        tail.shape('square')
+        tail.color('blue')
+        tail.penup()
+        body.append(tail)
+
+    move()
+
+    time.sleep(delay)
 
 screen.mainloop()
