30f4a86196a9dcf934817b394b527a907514986f
Bilong Chen <andrew.blchen@gmail.com>
2020-12-19 17:50:55 -0800

    Create pygame-template-week15.py

---
+++ pygame-template-week15.py
@@ -0,0 +1,13 @@
+import pygame
+
+print("Initialization")
+pygame.init()
+
+screen = pygame.display.set_mode((400, 300))
+pygame.display.update()
+
+# Game loop
+# Event processing
+# When quit is clicked, what to do?
+# When mouse is clicked, what to do?
+# When keyboard is pressed, what to do?
