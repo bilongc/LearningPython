79a3d3a3b799a3685fd1dbd6b633ba04d9bacb0c
Bilong Chen <andrew.blchen@gmail.com>
2021-01-09 15:05:15 -0800

    Update pygame-template-week15.py

--- pygame-template-week15.py
+++ pygame-template-week15.py
@@ -1,13 +1,45 @@
 import pygame
+import random
 
-print("Initialization")
-pygame.init()
+WIDTH = 360
+HEIGHT = 480
+FPS = 30
 
-screen = pygame.display.set_mode((400, 300))
-pygame.display.update()
+# Define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
 # Game loop
 # Event processing
 # When quit is clicked, what to do?
 # When mouse is clicked, what to do?
 # When keyboard is pressed, what to do?
+
+# Initialize pygame and create window
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Template")
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        # Check for closing window event
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Update
+
+    # Draw / Render
+    screen.fill(BLACK)
+
+    # After drawing everything, flip the display
+    pygame.display.flip()
+
+pygame.quit()
+
